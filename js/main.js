478916a3f8409c467a0583807ded543a89cab8cb
sagehartly <sage.hartly@gmail.com>
2016-04-05 08:55:12 -0400

    tried to add btn-active to css

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ var $svg = $('svg');
 $('.btn').on('click', function () {
   $('.lines').addClass('lineEngage');
   $('.lines').removeClass('lines');
-  $('.engage').addClass('is-engaged');
+  $('.engage').addClass('btn:active');
 });
